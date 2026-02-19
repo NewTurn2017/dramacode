726af295e287b7fb7a73208bd204df8802746f85
newTurn2017 <hyuni2020@gmail.com>
2026-02-19 15:18:32 +0900

    release: v0.5.3 Entire-Checkpoint: df14af4542aa

--- packages/dramacode/src/index.ts
+++ packages/dramacode/src/index.ts
@@ -7,7 +7,7 @@ import { ChatCommand } from "./cli/cmd/chat"
 import { DramaCommand } from "./cli/cmd/drama"
 import { Server } from "./server/server"
 
-export const VERSION = "0.5.2"
+export const VERSION = "0.5.3"
 Server.setVersion(VERSION)
 
 process.on("unhandledRejection", (e) => {
